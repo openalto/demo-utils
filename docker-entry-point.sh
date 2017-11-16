0182be4631636f038a59419ef4a7a202dc278c87
= <=>
2017-11-16 23:13:14 +0800

    Change config files and some scripts
    Signed-off-by: = <=>

--- docker-entry-point.sh
+++ docker-entry-point.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo 'Configuring features repositories and boot...'
-sed -i 's_^\(featuresRepositories =.*\)$_\1,mvn:org.opendaylight.alto.ext/alto-bwmonitor-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features,mvn:org.opendaylight.alto.ext/alto-pathmanager-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features,mvn:org.opendaylight.alto.ext/alto-unicorn-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features_' /opt/opendaylight/etc/org.apache.karaf.features.cfg
-sed -i 's_^\(featuresBoot =.*\)$_\1,odl-l2switch-switch,odl-alto-unicorn-rest' /opt/opendaylight/etc/org.apache.karaf.features.cfg
+sed -i 's_^\(featuresRepositories *=.*\)$_\1,mvn:org.opendaylight.alto.ext/alto-bwmonitor-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features,mvn:org.opendaylight.alto.ext/alto-pathmanager-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features,mvn:org.opendaylight.alto.ext/alto-unicorn-features/0.1.0.Boron-SR4-SNAPSHOT/xml/features_' /opt/opendaylight/etc/org.apache.karaf.features.cfg
+sed -i 's_^\(featuresBoot *=.*\)$_\1,odl-l2switch-switch,odl-alto-unicorn-rest_' /opt/opendaylight/etc/org.apache.karaf.features.cfg
+#sed -i 's_^\(featuresBoot *=.*\)$_\1,odl-openflowplugin-flow-services-rest_' /opt/opendaylight/etc/org.apache.karaf.features.cfg
 
 echo 'Starting OpenDaylight...'
 /opt/opendaylight/bin/start
--- run-unicorn.sh
+++ run-unicorn.sh
@@ -12,9 +12,10 @@ sudo docker run -d \
 	-p $port2:6653 \
 	-p $http_port:80 \
 	-p $api_port:8181 \
-    --entrypoint docker-entry-point.sh \
+	--entrypoint /docker-entry-point.sh \
+	-v $(pwd)/docker-entry-point.sh:/docker-entry-point.sh \
 	-v $(pwd)/$config_folder/web.xml:/opt/unicorn-server/WEB-INF/web.xml \
 	-v $(pwd)/$config_folder/server.json:/opt/unicorn-server/WEB-INF/classes/server.json \
 	-v $(pwd)/$config_folder/mock.json:/opt/unicorn-server/WEB-INF/classes/adapter/mock.json \
-	-v $(pwd)/$config_folder/orchestrator.json\:/opt/unicorn-server/WEB-INF/classes/orchestrator/orchestrator.json \
+	-v $(pwd)/$config_folder/orchestrators.json:/opt/unicorn-server/WEB-INF/classes/orchestrator/orchestrators.json \
 	fno2010/unicorn-server:odl-boron-sr4
