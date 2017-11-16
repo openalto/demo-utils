213fdae95a937a339ce8aabd52f337b77ba18f3f
Y.Jace Liu <yang.jace.liu@linux.com>
2017-11-17 02:50:11 +0800

    Update ports to subscribe
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- subscribe-all.sh
+++ subscribe-all.sh
@@ -2,6 +2,6 @@
 
 ./subscribe-ports.sh 1 openflow:12:1
 ./subscribe-ports.sh 2 openflow:7:1
-./subscribe-ports.sh 3 openflow:9:1 openflow:9:2 openflow:11:2
-./subscribe-ports.sh 4 openflow:1:3 openflow:1:1
+./subscribe-ports.sh 3 openflow:9:1 openflow:9:2 openflow:11:2 openflow:10:1
+./subscribe-ports.sh 4 openflow:1:3 openflow:1:1 openflow:3:2
 ./subscribe-ports.sh 5 openflow:5:1
