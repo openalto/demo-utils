1f446d09f79347b2417adb13011f0e818f81fce1
Y. Jace Liu <yang.jace.liu@linux.com>
2018-09-03 15:56:30 +0800

    Remove redundent files
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- generate-input.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<input xmlns="urn:opendaylight:flow:service">
-  <barrier>false</barrier>
-  <node xmlns:inv="urn:opendaylight:inventory">/inv:nodes/inv:node[inv:id="'$1'"]</node>
-  <cookie>'$3'</cookie>
-  <table_id>'$2'</table_id>
-</input>'
--- remove-flow.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONTROLLER=${1:-localhost}
-
-while read line
-do
-    echo "Removing flows about $line"
-    curl -v -u admin:admin -X POST -d "$(./generate-input.sh $line)" -H 'Content-Type: application/xml' http://$CONTROLLER:8181/restconf/operations/sal-flow:remove-flow
-done < "${2:-/dev/stdin}"
