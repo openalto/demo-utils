f9757a53eed3296bd2f159604c14726abce8c540
Y. Jace Liu <yang.jace.liu@linux.com>
2018-09-11 11:49:25 +0000

    Some modifications
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- ec2-utils/setup-path.sh
+++ ec2-utils/setup-path.sh
@@ -2,14 +2,14 @@
 
 id=${1:-1}
 #port=$((8181+$id))
-address_array=('172.31.5.84:8181', '172.31.5.236:8181', '172.31.12.73:8181')
+address_array=('172.31.5.84:8181' '172.31.5.236:8181' '172.31.12.73:8181')
 address=${address_array[$id]}
 port=8181
 config_folder=config_$id
 path_id=${2:-1}
 path_json=path-$path_id.json
 
-PYUNICORN_PATH=/usr/local/bin
+PYUNICORN_PATH=$HOME
 
 data=`cat $(pwd)/$config_folder/$path_json`
 echo $data
