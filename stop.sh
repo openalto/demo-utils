be148ae8da1e6cb34ca1bc852aa19c7a74042e4a
Y. Jace Liu <yang.jace.liu@linux.com>
2018-11-03 21:42:56 +0800

    Add script to stop services
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

---
+++ stop.sh
@@ -0,0 +1,14 @@
+WORK_HOME=$HOME
+
+# Stop opendaylight
+$WORK_HOME/opendaylight/bin/stop
+
+# Stop mininet
+sudo kill $(ps aux | grep xdom-mn | awk '{print $2}')
+sudo mn -c
+
+# Stop domain-agent
+sudo kill $(ps aux | grep jetty | awk '{print $2}')
+
+# Stop UnicornUI and orchestrator
+sudo kill $(ps aux | grep gunicorn | awk '{print $2}')
