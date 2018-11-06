4f68998b5cb44afea58a791dd39d11edd3f203ee
Y. Jace Liu <yang.jace.liu@linux.com>
2018-11-06 14:53:43 +0000

    Update stop scripts
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 WORK_HOME=$HOME
 
 # Stop opendaylight
-$WORK_HOME/opendaylight/bin/stop
+#$WORK_HOME/opendaylight/bin/stop
+sudo kill $(ps aux | grep karaf | awk '{print $2}')
 
 # Stop mininet
 sudo kill $(ps aux | grep xdom-mn | awk '{print $2}')
