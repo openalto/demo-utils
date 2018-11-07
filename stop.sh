2adb1d539ecf37cf24c7139e47fcf7ef59322641
Y. Jace Liu <yang.jace.liu@linux.com>
2018-11-07 15:15:04 +0800

    Update site A ip address
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- stop.sh
+++ stop.sh
@@ -2,7 +2,7 @@ WORK_HOME=$HOME
 
 # Stop opendaylight
 #$WORK_HOME/opendaylight/bin/stop
-sudo kill $(ps aux | grep karaf | awk '{print $2}')
+sudo kill -9 $(ps aux | grep karaf | awk '{print $2}')
 
 # Stop mininet
 sudo kill $(ps aux | grep xdom-mn | awk '{print $2}')
