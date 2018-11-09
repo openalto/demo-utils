d05c0916ea3d0cfd1718699765e2b19aeb38ff9f
Y. Jace Liu <yang.jace.liu@linux.com>
2018-11-09 19:56:02 +0800

    Update the script to fetch topology
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- fetch-topologies.sh
+++ fetch-topologies.sh
@@ -1,7 +1,7 @@
-function fetch_topology{
+function fetch_topology {
     hostname=$1
     http_port=$2
-    curl -u admin:admin http://$hostname:$http_port/restconf/operational/network-topology:network-topology | jq . > $(hostname)_topo.json
+    curl -u admin:admin http://$hostname:$http_port/restconf/operational/network-topology:network-topology | jq . > $hostname"_topo.json"
 }
 
 fetch_topology unicorn1 8181
