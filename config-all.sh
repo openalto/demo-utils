7a9d37b884a035f89a87fb430930b095f0917130
Y. Jace Liu <yang.jace.liu@linux.com>
2018-11-11 12:23:36 +0800

    merge subscribing and setting bandwidth to one script
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

---
+++ config-all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+function get_all_ports() {
+    hostname=$1
+    http_port=$2
+    ports=$(curl -s -u admin:admin http://$hostname:$http_port/restconf/operational/opendaylight-inventory:nodes | jq -r '.nodes.node[]."node-connector"[].id|select(endswith("LOCAL")|not)')
+    echo $ports
+}
+
+function subscribe_port() {
+    hostname=$1
+    http_port=$2
+
+    ports=$(get_all_ports $hostname $http_port)
+
+    for port in $ports; do
+        curl -v -u admin:admin http://$hostname:$http_port/restconf/operations/alto-bwmonitor:bwmonitor-subscribe -H 'Content-type: application/json' -d '{"input":{"port-id":["'$port'"]}}'
+    done
+}
+
+function config_capacity() {
+    IP=$1
+    HTTP_PORT=$2
+    CAPACITY=$3
+
+    shift
+
+    for port in $@ ; do
+        curl -v -X PUT -u admin:admin http://$IP:$HTTP_PORT/restconf/config/alto-bwmonitor:config-capacity/capacity/$port -H 'Content-type: application/json' -d '{"capacity": {"port-id":"'$port'", "capacity": '$CAPACITY'}}'
+    done
+}
+
+subscribe_port unicorn1 8181
+subscribe_port unicorn2 8181
+config_capacity unicorn1 8181 100000 $(get_all_ports unicorn1 8181)
+config_capacity unicorn2 8181 100000 $(get_all_ports unicorn2 8181)
---
+++ test-utils/test_resource.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+auth = ('admin', 'admin')
+
+def test_bwmonitor_subscribe(hostname, port_ids=["openflow:1:1"]):
+    raw_data = {
+        "input": {
+            "port-id": port_ids
+        }
+    }
+    url = "http://" + hostname + ":8181/restconf/operations/alto-bwmonitor:bwmonitor-subscribe"
+    print(url)
+    r = requests.post(url, json=raw_data, auth=auth)
+    print(r.text)
+    return r
+
+def test_bwmonitor_query(hostname, port_ids=["openflow:1:1"]):
+    raw_data = {
+        "input": {
+            "port-id": port_ids
+        }
+    }
+    url = "http://" + hostname + ":8181/restconf/operations/alto-bwmonitor:bwmonitor-query"
+    print(url)
+    r = requests.post(url, json=raw_data, auth=auth)
+    print(r.text)
+    return r
+
+def test_resource_query(hostname):
+    raw_data = {
+    "query-desc": [
+        {
+        "flow": {
+            "flow-id": 1,
+            "src-ip": "10.0.1.101",
+            "dst-ip": "10.0.2.201",
+            "dst-port": 2345
+        },
+        "ingress-point": ""
+        },
+        {
+        "flow": {
+            "flow-id": 2,
+            "src-ip": "10.0.1.102",
+            "dst-ip": "10.0.3.202",
+            "dst-port": 4567
+        },
+        "ingress-point": ""
+        }
+    ]
+    }
+    resp = requests.post("http://" + hostname + ":8080/experimental/v1/unicorn/resource-query", json=raw_data)
+    print(resp.text)
+    return resp
+
+if __name__ == '__main__':
+    #test_bwmonitor_subscribe("unicorn1", ["host:00:00:00:00:00:01/openflow:1:1"])
+    #test_bwmonitor_query("unicorn1", port_ids=["host:00:00:00:00:00:01/openflow:1:1"])
+    test_resource_query("unicorn1")
+    test_resource_query("unicorn2")
+    test_resource_query("unicorn3")
